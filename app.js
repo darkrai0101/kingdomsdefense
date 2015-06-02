ccb041d2abf95f785304ece61830050406d5222b
darkrai0101 <nv.trung91@gmail.com>
2015-06-02 16:47:09 -0700

    sua port

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var path = require('path');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3333);
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
